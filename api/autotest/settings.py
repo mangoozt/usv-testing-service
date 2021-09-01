413f634c8214619c4be32440a7073382bc2c73fc
Mark Tartagashev <mangoozt@gmail.com>
2021-09-01 20:58:44 +0300

    Moved to postgresql

--- api/autotest/settings.py
+++ api/autotest/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'autotest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': '5432',
     }
 }
 
