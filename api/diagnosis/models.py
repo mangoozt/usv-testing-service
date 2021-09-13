60835432bf64bc4d8ba0e43d3bc999cdcc5f2006
Mark Tartagashev <mangoozt@gmail.com>
2021-09-13 16:21:07 +0300

    Add fields to testing recording

--- api/diagnosis/models.py
+++ api/diagnosis/models.py
@@ -16,6 +16,8 @@ class TestingRecording(models.Model):
     date = models.DateTimeField(default=datetime.datetime.now)
     file = models.FileField(upload_to='')
     title = models.TextField(default="", max_length=1000)
+    commit_sha1 = models.TextField(default="", max_length=40)
+    commit_date = models.DateTimeField(default=None, null=True)
     # We will storage all plots data in string format using '::' delimiter
     code0 = models.TextField(blank=True, default='', max_length=2000)
     code1 = models.TextField(blank=True, default='', max_length=2000)
