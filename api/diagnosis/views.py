277a97ddef701b846503bb9ddc987022fe7512f1
Mark Tartagashev <mangoozt@gmail.com>
2021-10-08 18:08:17 +0300

    Fix 'NoneType' object has no attribute 'f2f'

--- api/diagnosis/views.py
+++ api/diagnosis/views.py
@@ -34,14 +34,15 @@ def details(request, slug):
     chart_data = [df.columns.tolist()] + df.values.tolist()
     percent_a = []
     solv_tr = [obj.f2f, obj.ovn, obj.ov, obj.gw, obj.sve, obj.gwp, obj.sp, obj.cm, obj.ci, obj.vrf, obj.vrb]
-    solv_sc = [obj.sc_set.f2f, obj.sc_set.ovn, obj.sc_set.ov, obj.sc_set.gw,
-               obj.sc_set.sve, obj.sc_set.gwp, obj.sc_set.sp, obj.sc_set.cm,
-               obj.sc_set.ci, obj.sc_set.vrf, obj.sc_set.vrb]
-    for i in range(len(solv_tr)):
-        try:
-            percent_a.append(solv_tr[i] / solv_sc[i] * 100)
-        except ZeroDivisionError:
-            percent_a.append(100)
+    if obj.sc_set is not None:
+        solv_sc = [obj.sc_set.f2f, obj.sc_set.ovn, obj.sc_set.ov, obj.sc_set.gw,
+                   obj.sc_set.sve, obj.sc_set.gwp, obj.sc_set.sp, obj.sc_set.cm,
+                   obj.sc_set.ci, obj.sc_set.vrf, obj.sc_set.vrb]
+        for i in range(len(solv_tr)):
+            try:
+                percent_a.append(solv_tr[i] / solv_sc[i] * 100)
+            except ZeroDivisionError:
+                percent_a.append(100)
     # TODO add stats
     rec = {"chart_data": json.dumps(list(chart_data), ensure_ascii=False),
            "date": str(obj.date),
